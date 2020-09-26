28fcce816f1a1b5bb31fe58ed8c5cf02b164785f
Edward Cho <eycho@andrew.cmu.edu>
2020-09-26 19:03:34 -0400

    Wrote selection sort and binary search

--- Searching.py
+++ Searching.py
@@ -4,40 +4,67 @@ Lecture 4 Search
 """
 
 # O(n)
-def isIn(x: int, A: Iterable[int], lo: int, hi: int): -> bool
+def isIn(x: int, A: Iterable[int], lo: int, hi: int) -> bool:
     # requires 0 <= lo && lo <= hi && hi <= len(A)
     for i in range(lo, hi):
-        #loop invariant lo <= i and i <= hi
+        assert lo <= i and i <= hi
         if (A[i] == x): return True
     return False
 
 # O(n)
-def isSorted(A: Iterable[int], lo: int, hi: int): -> bool
+def isSorted(A: Iterable[int], lo: int, hi: int) -> bool:
     # requires 0 <= lo && lo <= hi && hi <= len(A)
     for i in range(lo, hi-1):
-        # loop_variant lo <= i
-        if (!A[i] <= A[i+1]): return False
+        assert lo <= i
+        if not (A[i] <= A[i+1]): return False
     return True
 
 
 #Linear Search
 # O(n)
-def linearSearch(x: int, A: Iterable[int], n: int): -> int
+def linearSearch(x: int, A: Iterable[int], n: int) -> int:
     """
+    Implementation of Linear Search
     requires 0 <= n and n <= len(A)
-    requires isSorted(A,0,n);
-    ensures (result == -1 and !isIn(x,A,0,n)) or 
+    requires isSorted(A,0,n)
+    ensures (result == None and not isIn(x,A,0,n)) or 
     ((0 <= result and result < n) and A[result] == x)
     """
     for i in range(n):
-        #loop_invariant 0 <= i and i <= n
-        #loop_invariant i == 0 || A[i-1] < x
-        if (A[i] > x): 
+        assert 0 <= i and i <= n
+        assert i == 0 or A[i-1] < x
+        if (A[i] > x):
             return None 
         if (A[i] == x): 
             return i
     #originally -1, but in Python it would return the end of the list
     return None
 
+# A = [0,2,5,6,12]
+# print(linearSearch(5,A,len(A)))
 
+#Binary Search
+#O(log(n))
+def binSearch(x:int, A:Iterable[int], n:int) -> int:
+    """
+    Implementation of Binary Search
+    requires 0 <= n and n <= len(A)
+    requires isSorted(A,0,n)
+    ensures (None == result and not isIn(x, A, 0, n)) or
+    ((0 <= result and result < n) and A[result] == x)
+    """
+    lo, hi = 0, n
+    while (lo < hi):
+        assert 0 <= lo and lo <= hi and hi <= n
+        assert (lo == 0 or A[lo-1] < x)
+        assert (hi == n or A[hi] > x)
+        mid = lo + (hi-lo)//2
+        if (A[mid] == x): 
+            return mid
+        if (A[mid] < x):
+            low = mid + 1
+        else: #A[mid] > x
+            hi = mid
+    return None
 
+# print(binSearch(5,A,len(A)))
--- Sort.py
+++ Sort.py
@@ -3,4 +3,74 @@ from typing import Iterable
 Lecture 5 Sorting
 """
 
-def selectionSort(A: Iterable[int], lo: int, hi: int): -> None
+#O(n)
+def isSorted(A: Iterable[int], lo: int, hi: int) -> bool:
+    """
+    Returns true if the array segment A[lo..hi] is sorted
+    requires 0 <= i and i < len(A)
+    requires 0 <= j and j < len(A)
+    """
+    if (lo == hi): return True
+    return lessThanSeg(A[lo], A, lo+1, hi) and isSorted(A, lo+1, hi)
+    
+#O(n)
+def lessThanSeg(x: int, A: Iterable[int], lo: int, hi: int) -> bool:
+    """
+    Returns true if x <= A[lo..hi]
+    """
+    if (lo == hi): return True
+    return (x <= A[lo] and lessThanSeg(x, A, lo+1, hi))
+
+#O(n)
+def lessThanSegs(A: Iterable[int], lo1: int, hi1: int, B: Iterable[int], lo2: int, hi2: int) -> bool:
+    """
+    Returns true if A[lo1..hi1] <= A[lo2..hi2]
+    """
+    if (lo1 == hi1): return True
+    return lessThanSeg(A[lo1], B, lo2, hi2) and lessThanSegs(A, lo1+1, hi1, B, lo2, hi2)
+
+#O(1)
+def swap(A: Iterable[int], i: int, j: int) -> None:
+    """
+    Swaps A[i] with A[j]
+    """
+    tmp = A[i]
+    A[i] = A[j]
+    A[j] = tmp
+
+#O(n)
+def findMin(A: Iterable[int], lo: int, hi: int) -> int:
+    """
+    Returns the index m of a minimal element in the non-empty segment A[lo..hi]
+    requires 0 <= lo and lo < hi and hi <= len(A)
+    ensures lo <= result and result < hi
+    ensures lessThanSeg(A[result], A, lo, hi)
+    """
+    min = lo
+    for i in range(lo+1, hi):
+        assert lo < i and i <= hi
+        assert lo <= min and min < hi
+        assert lessThanSeg(A[min], A, lo, i)
+        if (A[i] < A[min]):
+            min = i
+    return min
+
+#O(n^2)
+def selectionSort(A: Iterable[int], lo: int, hi: int) -> None:
+    """
+    Implementation of selection sort
+    requires 0 <= lo and lo <= hi and hi <= len(A)
+    ensures isSorted(A, lo, hi)
+    """
+    for i in range(hi):
+        assert lo <= i and i <= hi
+        assert isSorted(A, lo, i)
+        assert lessThanSegs(A, lo, i, A, i, hi)
+        min = findMin(A, i, hi)
+        swap(A, i, min)
+
+
+# A = [1,5,6,7,3,4]
+# print(A)
+# selectionSort(A, 0, len(A))
+# print(A)
