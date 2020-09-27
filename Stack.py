2f9f7a71218f41fc5db72cc941669afbb60d0deb
Edward Cho <eycho@andrew.cmu.edu>
2020-09-26 23:21:13 -0400

    Made separate class for Node

--- Stack.py
+++ Stack.py
@@ -1,32 +1,5 @@
 from typing import Iterable
-
-class Node:
-    def __init__(self, data: str, next: 'Node'=None):
-        self.data = data
-        self.next = next
-    
-    # O(1)
-    def getNext(self) -> 'Node':
-        return self.next
-
-    # O(1)
-    def getData(self) -> str:
-        return self.data
-
-    # O(1)
-    def setNext(self, next: 'Node') -> None:
-        self.next = next
-
-    # O(1)
-    def setData(self, data: str) -> None:
-        self.data = data
-    
-    # O(n)
-    @staticmethod
-    def isSegmentSlit(start: 'Node', end: 'Node'):
-        if (start == None): return False
-        if (start == end): return True
-        return isSegmentSlit(start.getNext(), end)
+import Node
     
 class Stack:
 
